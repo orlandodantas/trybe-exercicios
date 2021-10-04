48e73c485892aeb3a5846a509b274f769db13723
Orlando Dantas <orlandodantas.1@hotmail.com>
2021-10-04 15:37:47 -0300

    exercicios de fixacao concluidos

---
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/exercicios-de-fixacao/script.js
@@ -0,0 +1,9 @@
+const acordando = () => 'Acordando!!';
+const tomarCafe = () => 'Bora tomar café!!';
+const dormir = () => 'Partiu dormir!!';
+
+const doingThings = (func) => console.log(func);
+
+doingThings(acordando());
+doingThings(tomarCafe());
+doingThings(dormir());
